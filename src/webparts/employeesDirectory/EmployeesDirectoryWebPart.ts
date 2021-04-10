fcf1198a58d7a6c3d9da997acfe9034132b0f5cb
a-albehwar <71848269+a-albehwar@users.noreply.github.com>
2021-04-10 09:26:29 +0000

    commit

--- src/webparts/employeesDirectory/EmployeesDirectoryWebPart.ts
+++ src/webparts/employeesDirectory/EmployeesDirectoryWebPart.ts
@@ -162,7 +162,7 @@ export default class EmployeesDirectoryWebPart extends BaseClientSideWebPart<IEm
 
     this.context.spHttpClient
       .get(
-        `${this.context.pageContext.web.absoluteUrl}/_api/search/query?querytext='((WorkEmail:*tecq8.onmicrosoft.com)+AND+(PreferredName:`+encodeURIComponent(searchPreferredName)+`)+AND+(Department:`+encodeURIComponent(searchKeywordDepartment)+`))'&selectproperties='AccountName,Department,JobTitle,Path,PictureURL,PreferredName,FirstName,WorkEmail,WorkPhone,SPS-PhoneticDisplayName,OfficeNumber'&sourceid='B09A7990-05EA-4AF9-81EF-EDFAB16C4E31'&sortlist='firstname:ascending'&rowLimit=1000`,
+        `${this.context.pageContext.web.absoluteUrl}/_api/search/query?querytext='((WorkEmail:*tecq8.com.kw)+AND+(PreferredName:`+encodeURIComponent(searchPreferredName)+`)+AND+(Department:`+encodeURIComponent(searchKeywordDepartment)+`))'&selectproperties='AccountName,Department,JobTitle,Path,PictureURL,PreferredName,FirstName,WorkEmail,WorkPhone,SPS-PhoneticDisplayName,OfficeNumber'&sourceid='B09A7990-05EA-4AF9-81EF-EDFAB16C4E31'&sortlist='firstname:ascending'&rowLimit=1000`,
         SPHttpClient.configurations.v1
       )
       .then((response) => {
