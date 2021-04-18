afac27f7371e7c78a5360679fbfb44fb7aedf150
SenaDiyarme <k.keerthi@diyarme.com>
2021-04-18 16:07:06 +0000

    updated fuad commnets

--- src/webparts/empSuggestionBox/components/EmpSuggestionBox.tsx
+++ src/webparts/empSuggestionBox/components/EmpSuggestionBox.tsx
@@ -38,8 +38,9 @@ export interface IListItem {
   Title?: string;  
   Id: number;  
 }  
- 
+var emp_name,emp_jobtitle,emp_dept,sug_title,sug_Desc,sug_Type;
 export default class EmpSuggestionBox extends React.Component<IEmpSuggestionBoxProps, any> {
+  private EmpDirectoryListName:string="EmployeeDirectory";
   private language:string;
   private userDepartment:string;
   private userJobTilte:string;
@@ -50,10 +51,12 @@ export default class EmpSuggestionBox extends React.Component<IEmpSuggestionBoxP
       fileInfos: null,
     };
     var searchPreferredmail=this.props.email;
-    
+
     //var searchPreferredmail="j.joshua@tec.com.kw"; // for testing done
+    this.getDataFromEmpDirectoryList(searchPreferredmail);
     
-    this.props.context.spHttpClient.get(`${this.props.siteurl}/_api/search/query?querytext=%27(WorkEmail:`+encodeURIComponent(searchPreferredmail)+`)%27&selectproperties=%27AccountName,Department,JobTitle,WorkEmail%27&sourceid=%27B09A7990-05EA-4AF9-81EF-EDFAB16C4E31%27&sortlist=%27firstname:ascending%27`,SPHttpClient.configurations.v1)
+    
+    /* this.props.context.spHttpClient.get(`${this.props.siteurl}/_api/search/query?querytext=%27(WorkEmail:`+encodeURIComponent(searchPreferredmail)+`)%27&selectproperties=%27AccountName,Department,JobTitle,WorkEmail%27&sourceid=%27B09A7990-05EA-4AF9-81EF-EDFAB16C4E31%27&sortlist=%27firstname:ascending%27`,SPHttpClient.configurations.v1)
     .then((response) => {
       return response.json().then((items: any): void => {
         //console.log('items.value: ', items.value);
@@ -74,10 +77,23 @@ export default class EmpSuggestionBox extends React.Component<IEmpSuggestionBoxP
             });
           });
       });
-    });
+    }); */
   }
 
-  
+  private getDataFromEmpDirectoryList(mail){
+    sp.site.rootWeb.lists.getByTitle(this.EmpDirectoryListName).items.filter("EmpEmail eq '"+mail+"'").getAll()
+    .then(r=>{
+      console.log(r);
+      if(r.length>0){
+        $("#txtEmpName").val(r[0].Title!=null?r[0].Title:"");
+        $("#txtjobtitle").val(r[0].EmpDesignation!=null?r[0].EmpDesignation:"");
+        $("#txtDept").val(r[0].EmpDepartment!=null?r[0].EmpDepartment:"");
+      }
+
+    }).catch(function(err) {  
+      console.log(err);  
+    });
+  }
  
   public render(): React.ReactElement<IEmpSuggestionBoxProps> {
     var weburl=this.props.weburl;
@@ -90,33 +106,50 @@ export default class EmpSuggestionBox extends React.Component<IEmpSuggestionBoxP
       <div className={"container-fluid mt-5"}>
           <div className={"col-md-10 mx-auto col-12"}>
               <div className={"row user-info"}>
-                  <h3 className={"mb-4 col-12"}>NEW SUGGESTION REQUEST</h3>
+                  <h3 className={"mb-4 col-12"}>Employee Details</h3>
+                  <div className="col-md-4 col-12 mb-4">
+                        <p>Name<span  style={errormsgStyle}>*</span></p>
+                        <input type="text" id="txtEmpName" className="form-input" name="txtEmpName" onBlur={() => this.validateTextBox('txtEmpName','Name is mandatory')}/>
+                        <label id="lbl_emp_name" className="form-label" style={errormsgStyle}></label>
+                  </div>
+                  <div className="col-md-4 col-12 mb-4">
+                        <p>Job Title<span  style={errormsgStyle}>*</span></p>
+                        <input type="text" id="txtjobtitle" className="form-input" name="txtjobtitle" onBlur={() => this.validateTextBox('txtjobtitle','Job Title is mandatory')}/>
+                        <label id="lbl_emp_jobtitle" className="form-label" style={errormsgStyle}></label>
+                  </div>
+                  <div className="col-md-4 col-12 mb-4">
+                        <p>Department<span  style={errormsgStyle}>*</span></p>
+                        <input type="text" id="txtDept" className="form-input" name="txtDept" onBlur={() => this.validateTextBox('txtDept','Department is mandatory')}/>
+                        <label id="lbl_emp_dept" className="form-label" style={errormsgStyle}></label>
+                  </div>
+                     
+                  <h3 className={"mb-4 col-12"}>Suggestion Details</h3>
                   <div className={"col-md-12 col-12 mb-4"}>
                       <p id="lbl_Sug_Type">{arrLang[lang]['SuggestionBox']['SugType']}<span  style={errormsgStyle}>*</span></p>
                       <div className="vleft">
-                          <input type="radio" id="rb_money" name="suggestionType" className={"form-control"} value="Save Money" onChange={this.handleradioClick}></input>
+                          <input type="radio" id="rb_money" name="suggestionType" className={"form-control"} value="Save Money" onChange={this.handleradioClick} onBlur={this.checkboxval}></input>
                           <label  id="lbl_money" className={"form-label"}>{arrLang[lang]['SuggestionBox']['SaveMoney']}</label>
-                          <input type="radio" id="rb_security" name="suggestionType" className={"form-control"} value="Improve Safety" onChange={this.handleradioClick}></input>
+                          <input type="radio" id="rb_security" name="suggestionType" className={"form-control"} value="Improve Safety" onChange={this.handleradioClick} onBlur={this.checkboxval}></input>
                           <label   id="lbl_security" className={"form-label"}>{arrLang[lang]['SuggestionBox']['ImproveSecurity']}</label><br></br>
-                          <input type="radio" id="rb_efficency" name="suggestionType" className={"form-control"} value="Improve Efficiency" onChange={this.handleradioClick}></input>
+                          <input type="radio" id="rb_efficency" name="suggestionType" className={"form-control"} value="Improve Efficiency" onChange={this.handleradioClick} onBlur={this.checkboxval}></input>
                           <label id="lbl_Efficency" className={"form-label"}>{arrLang[lang]['SuggestionBox']['ImporveEfficiency']}</label>
-                          <input type="radio" id="rb_other" name="suggestionType" className={"form-control"} value="Other" onChange={this.handleradioClick}></input>
+                          <input type="radio" id="rb_other" name="suggestionType" className={"form-control"} value="Other" onChange={this.handleradioClick} onBlur={this.checkboxval}></input>
                           <label id="lbl_Other" className={"form-label"}>{arrLang[lang]['SuggestionBox']['Other']}</label><br></br>              
                       </div>
                       <label id="lbl_SugTypeerr" className={"form-label"}  style={errormsgStyle}></label>
                   </div>
                   <div id='div_other' className={"col-lg-4  mb-2"} style={displayStyle}>
                     <p id="lbl_Other">{arrLang[lang]['SuggestionBox']['Other']}<span  style={errormsgStyle}>*</span></p>                 
-                    <input type="text" id="txt_other" className={"form-control"} name="other" placeholder={arrLang[lang]['SuggestionBox']['Other']}/>
+                    <input type="text" id="txt_other" className={"form-input"} name="other" placeholder={arrLang[lang]['SuggestionBox']['Other']}/>
                   </div>
                   <div className="col-md-12 col-12 mb-4">
                       <p>{arrLang[lang]['SuggestionBox']['Title']}<span  style={errormsgStyle}>*</span></p>
-                      <textarea id="idTitle"  style={textareaStyle} cols={115} rows={10}  name="Suggesstiontitle" placeholder={arrLang[lang]['SuggestionBox']['Title']}></textarea>
+                      <textarea id="idTitle"   className="form-input" style={textareaStyle} cols={115} rows={10}  name="Suggesstiontitle" placeholder={arrLang[lang]['SuggestionBox']['Title']}  onBlur={() => this.validateTextBox('idTitle',arrLang[lang]['SuggestionBox']['Title'])}></textarea>
                       <label id="lbl_subjecterr" className={"form-label"}  style={errormsgStyle}></label>
                   </div>
                   <div className={"col-md-12 col-12 mb-4"}>
                       <p>{arrLang[lang]['SuggestionBox']['Description']} <span  style={errormsgStyle}>*</span></p>
-                      <textarea id="idSuggestion" style={textareaStyle} cols={115} rows={10}  name="Suggesstion" placeholder={arrLang[lang]['SuggestionBox']['TypeMessagehere']}></textarea>
+                      <textarea id="idSuggestion"  className="form-input" style={textareaStyle} cols={115} rows={10}  name="Suggesstion" placeholder={arrLang[lang]['SuggestionBox']['TypeMessagehere']}  onBlur={() => this.validateTextBox('idSuggestion',arrLang[lang]['SuggestionBox']['Description'])}></textarea>
                       <label id="lbl_suggestionerr" className={"form-label"} style={errormsgStyle}></label>
                   </div>
                   
@@ -140,7 +173,7 @@ export default class EmpSuggestionBox extends React.Component<IEmpSuggestionBoxP
           <div className="col-md-10 mx-auto col-12">
               <div className="row">
                 <div className=" col-12 btnright">
-                      <button className="red-btn shadow-sm  ml-4"  id="btnSubmit"   onClick={this.upload.bind(this)}>{arrLang[lang]['SuggestionBox']['Submit']}</button>
+                      <button className="red-btn shadow-sm  ml-4"  id="btnSubmit"   onClick={this.CreateSuggestion.bind(this)}>{arrLang[lang]['SuggestionBox']['Submit']}</button>
                       <button className="red-btn shadow-sm  ml-4"  id="btnCancel"  onClick={(e) => {
                                     e.preventDefault();
                                     window.location.href=weburl;
@@ -169,23 +202,31 @@ export default class EmpSuggestionBox extends React.Component<IEmpSuggestionBoxP
         document.getElementById("div_other").style.display = 'none';
         //Hide textbox.
         }
-    }
-
-  private _validateSubject(value: string): string {
-    if (value.length <= 0) {
-      return arrLang[lang]['SuggestionBox']['SugTitleError'];
-    }
-    else {
-      return " ";
-    }
   }
 
-  private _validateDescription(value: string): string {
-    if (value.length <= 0) {
-      return arrLang[lang]['SuggestionBox']['SugDescError'];
+  private validateTextBox(e,errmsg){
+    //const inputElement = e.target as HTMLInputElement;
+     var inputval=$('#'+e).val();
+     var inputspan=$('#'+e).next("label");
+     if(inputval=="")
+     {
+       inputspan.text(errmsg);
+   
+     }
+     else
+     {
+       inputspan.text("");
+     }
+   }
+  private checkboxval(){
+    var sel_val=$('input[name="suggestionType"]:checked').val();
+    if(sel_val==null || sel_val=="" || sel_val==undefined)
+    {
+      $("#lbl_SugTypeerr").text("");
+      document.getElementById('lbl_SugTypeerr').append(arrLang[lang]['SuggestionBox']['SugTypeError']);
     }
-    else {
-      return " ";
+    else{
+      $("#lbl_SugTypeerr").text("");
     }
   }
 
@@ -219,32 +260,90 @@ export default class EmpSuggestionBox extends React.Component<IEmpSuggestionBoxP
     this.setState({fileInfos});
     console.log(fileInfos)
   }
+  private validateForm(){
+    var isvalidform=true;
+     emp_name=$("#txtEmpName").val();
+     emp_jobtitle=$("#txtjobtitle").val();
+     emp_dept=$("#txtDept").val();
 
-  private upload(event) {
-    var sug_title=$("#idTitle").val();
-    var sug_Desc=$("#idSuggestion").val();
-    var sug_Type= $('input[name="suggestionType"]:checked').val();
-    //alert(sug_Type);
-    $("#lbl_subjecterr").empty();
-    document.getElementById('lbl_subjecterr').append(this._validateSubject( sug_title));
-    $("#lbl_suggestionerr").empty();
-    document.getElementById('lbl_suggestionerr').append(this._validateDescription(sug_Desc));
-    $("#lbl_SugTypeerr").empty();
+     sug_title=$("#idTitle").val();
+     sug_Desc=$("#idSuggestion").val();
+     sug_Type= $('input[name="suggestionType"]:checked').val();
 
-    if(sug_Type==null || sug_Type=="" || sug_Type==undefined)
+     if(emp_name=="" || emp_name==undefined)
+     {
+       $("#lbl_emp_name").text("Name is mandatory");
+         isvalidform = false;
+     }
+     else
+     {
+       $("#lbl_emp_name").text("");
+     }
+
+     if(emp_jobtitle=="")
+     {
+       $("#lbl_emp_jobtitle").text("Job Title is mandatory");
+         isvalidform = false;
+     }
+     else
+     {
+       $("#lbl_emp_jobtitle").text("");
+     }
+
+     if(emp_dept=="")
+     {
+       $("#lbl_emp_dept").text("Department is mandatory");
+         isvalidform = false;
+     }
+     else
+     {
+       $("#lbl_emp_dept").text("");
+     }
+     if(sug_title=="")
+     {
+       $("#lbl_subjecterr").text(arrLang[lang]['SuggestionBox']['Title']);
+         isvalidform = false;
+     }
+     else
+     {
+       $("#lbl_subjecterr").text("");
+     }
+     if(sug_Desc=="")
+     {
+       $("#lbl_suggestionerr").text(arrLang[lang]['SuggestionBox']['Description']);
+         isvalidform = false;
+     }
+     else
+     {
+       $("#lbl_suggestionerr").text("");
+     }
+
+     if(sug_Type==null || sug_Type=="" || sug_Type==undefined)
     {
+      $("#lbl_SugTypeerr").text("");
       document.getElementById('lbl_SugTypeerr').append(arrLang[lang]['SuggestionBox']['SugTypeError']);
+      isvalidform = false;
     }
     else if(sug_Type=="Other"){
       if($("#txt_other").val()==""){
+        $("#lbl_SugTypeerr").text("");
         document.getElementById('lbl_SugTypeerr').append(arrLang[lang]['SuggestionBox']['SugTypeError']);
+        isvalidform = false;
       }
       else{
       sug_Type=$("#txt_other").val();
+      $("#lbl_SugTypeerr").text("");
       }
     }
+    else{
+      $("#lbl_SugTypeerr").text("");
+    }
     
-    if(sug_title !="" && sug_Desc !="" && sug_Type!=""){
+      return isvalidform;
+  }
+  private CreateSuggestion(event) {
+   
+    if(this.validateForm()==true){
 
       let {fileInfos}=this.state;
        if(lang=="en"){
@@ -253,15 +352,16 @@ export default class EmpSuggestionBox extends React.Component<IEmpSuggestionBoxP
               Description:sug_Desc,
               Suggestion_StatusId: 1,
               Suggestion_Type:sug_Type,
-              User_JobTitle:this.userJobTilte,
-              User_Department:this.userDepartment,
+              User_JobTitle:emp_jobtitle,
+              User_Department:emp_dept,
+              User_Name:emp_name
             }).then(r=>{
               // if(fileInfos!=null){
               // r.item.attachmentFiles.addMultiple(fileInfos);
              
               // }
               //this.updateLogs(r.data.Id,r.data.AuthorId);
-              alert( arrLang[lang]['SuggestionBox']['SuccessMsg']);
+              alert("Thank you ! Your request was submitted Successfully");
               window.location.href=this.props.weburl;
             }).catch(function(err) {  
               console.log(err);  
@@ -276,14 +376,15 @@ export default class EmpSuggestionBox extends React.Component<IEmpSuggestionBoxP
               //Description_Ar:sug_Desc,
               Suggestion_StatusId: 1,
               Suggestion_Type:sug_Type,
-              User_JobTitle:this.userJobTilte,
-              User_Department:this.userDepartment,
+              User_JobTitle:emp_jobtitle,
+              User_Department:emp_dept,
+              User_Name:emp_name
             }).then(r=>{
               // if(fileInfos!=null){
               // r.item.attachmentFiles.addMultiple(fileInfos);
               // }
              // this.updateLogs(r.data.Id,r.data.AuthorId);
-             alert( arrLang[lang]['SuggestionBox']['SuccessMsg']);
+             alert( "Thank you ! Your request was submitted Successfully");
              window.location.href=this.props.weburl;
             }).catch(function(err) {  
               console.log(err);  
@@ -291,7 +392,7 @@ export default class EmpSuggestionBox extends React.Component<IEmpSuggestionBoxP
        }
     }
     else{
-      //alert(arrLang[lang]['SuggestionBox']['FillMandatoryFields']);
+      alert("Sorry,Please check your form where some data is not in a valid format.");
       event.preventDefault();
       return false;
     }  
